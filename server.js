056be95a76b5d53e7fa990a94ec761d3c6d887c6
Carlos Hernandez <carloshdmn@gmail.com>
2019-07-24 18:43:43 -0500

    Clase 29

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const socket = require('./socket');
 const db = require('./db');
 const router = require('./network/routes');
 
 db('mongodb://user:user1234@ds255107.mlab.com:55107/telegrom');
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
