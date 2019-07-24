513ac0a73ab4b31ebd2eb87426a4c8aecc10b43f
Carlos Hernandez <carloshdmn@gmail.com>
2019-07-24 14:37:23 -0500

    Clase 24

--- components/chat/model.js
+++ components/chat/model.js
@@ -3,16 +3,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const mySchema = new Schema({
-    user: {
+    users: [{
         type: Schema.ObjectId,
         ref: 'User',
-    },
-    message: {
-        type: String,
-        required: true,
-    },
-    date: Date,
+    }],
 });
 
-const model = mongoose.model('Message', mySchema);
+const model = mongoose.model('Chat', mySchema);
 module.exports = model;
--- components/message/store.js
+++ components/message/store.js
@@ -6,12 +6,22 @@ function addMessage(message) {
 }
 
 async function getMessages(filterUser) {
-    let filter = {};
-    if (filterUser !== null) {
-        filter = { user: filterUser };
-    }
-    const messages = await Model.find(filter);
-    return messages;
+    return new Promise((resolve, reject) => {
+        let filter = {};
+        if (filterUser !== null) {
+            filter = { user: filterUser };
+        }
+        Model.find(filter)
+            .populate('user')
+            .exec((error, populated) => {
+                if (error) {
+                    reject(error);
+                    return false;
+                }
+
+                resolve(populated);
+            });
+    })
 }
 
 function removeMessage(id) {
