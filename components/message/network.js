1df71382ea4837fc5abe054453a3e08d72e42341
Carlos Hernandez <carloshdmn@gmail.com>
2019-07-24 15:07:33 -0500

    Clase 25

--- components/message/network.js
+++ components/message/network.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const multer = require('multer');
+
 const response = require('../../network/response');
 const controller = require('./controller');
 const router = express.Router();
+
+const upload = multer({
+    dest: 'uploads/',
+});
 
 router.get('/', function (req, res) {
     const filterMessages = req.query.chat || null;
@@ -13,7 +19,7 @@ router.get('/', function (req, res) {
             response.error(req, res, 'Unexpected Error', 500, e);
         })
 });
-router.post('/', function (req, res) { 
+router.post('/', upload.single('file'), function (req, res) { 
     controller.addMessage(req.body.chat, req.body.user, req.body.message)
         .then((fullMessage) => {
             response.success(req, res, fullMessage, 201);    
